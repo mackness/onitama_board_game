3a066e2c5a88e46cbcf5e7e2de6b9c4ca09fbff7
Mack Solomon <msolomon@eharmony.com>
2018-03-14 07:16:32 -0700

    update card data and add dark green card type

--- src/cards/index.ts
+++ src/cards/index.ts
@@ -2,19 +2,43 @@ import c from '../constants/game-constants';
 
 export default [
 	{
-		school: 'mantis',
+		school: 'goose',
 		color: 'blue',
-		player: c.RED,
+		player: c.BLUE,
 		card: [
 			[0, 0, 0, 0, 0],
-			[0, 0, c.MOVE, 0, 0],
+			[0, c.MOVE, c.MOVE, 0, 0],
 			[0, 0, c.START, 0, 0],
-			[0, c.MOVE, 0, c.MOVE, 0],
+			[0, 0, c.MOVE, c.MOVE, 0],
 			[0, 0, 0, 0, 0]
 		]
 	},
 	{
-		school: 'ox',
+		school: 'frog',
+		color: 'blue',
+		player: c.RED,
+		card: [
+			[0, 0, c.MOVE, 0, 0],
+			[0, 0, 0, c.MOVE, 0],
+			[0, 0, c.START, 0, 0],
+			[0, 0, 0, c.MOVE, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'eel',
+		color: 'blue',
+		player: c.BLUE,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, c.MOVE, 0, c.MOVE, 0],
+			[0, 0, c.START, 0, 0],
+			[0, 0, c.MOVE, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'horse',
 		color: 'blue',
 		player: c.RED,
 		card: [
@@ -26,38 +50,146 @@ export default [
 		]
 	},
 	{
+		school: 'boar',
+		color: 'green',
+		player: c.RED,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, c.MOVE, 0, 0],
+			[0, c.MOVE, c.START, c.MOVE, 0],
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'mantis',
+		color: 'green',
+		player: c.RED,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, c.MOVE, 0, 0, 0],
+			[0, 0, c.START, c.MOVE, 0],
+			[0, c.MOVE, 0, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'crab',
+		color: 'green',
+		player: c.BLUE,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, c.MOVE, 0, 0],
+			[c.MOVE, 0, c.START, 0, c.MOVE],
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'dragon',
+		color: 'green',
+		player: c.RED,
+		card: [
+			[0, 0, 0, 0, 0],
+			[c.MOVE, 0, 0, 0, c.MOVE],
+			[0, 0, c.START, 0, 0],
+			[0, c.MOVE, 0, c.MOVE, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
 		school: 'cobra',
-		color: 'green',
-		player: c.RED,
-		card: [
-			[0, 0, 0, 0, 0],
-			[0, c.MOVE, 0, c.MOVE, 0],
-			[0, 0, c.START, 0, 0],
-			[0, 0, c.MOVE, 0, 0],
-			[0, 0, 0, 0, 0]
-		]
-	},
-	{
-		school: 'horse',
-		color: 'green',
-		player: c.RED,
-		card: [
-			[0, 0, 0, 0, 0],
-			[0, c.MOVE, 0, c.MOVE, 0],
-			[0, 0, c.START, 0, 0],
-			[0, 0, c.MOVE, 0, 0],
-			[0, 0, 0, 0, 0]
-		]
-	},
-	{
-		school: 'eel',
 		color: 'red',
 		player: c.RED,
 		card: [
 			[0, 0, 0, 0, 0],
+			[0, 0, c.MOVE, 0, 0],
+			[0, 0, c.START, 0, 0],
+			[0, c.MOVE, 0, c.MOVE, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'ox',
+		color: 'red',
+		player: c.BLUE,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[0, c.MOVE, c.START, c.MOVE, 0],
+			[0, 0, c.MOVE, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'rooster',
+		color: 'red',
+		player: c.RED,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, c.MOVE, c.MOVE, 0],
+			[0, 0, c.START, 0, 0],
+			[0, c.MOVE, c.MOVE, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'rabbit',
+		color: 'red',
+		player: c.BLUE,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, c.MOVE, 0],
+			[0, 0, c.START, 0, 0],
+			[0, c.MOVE, 0, 0, 0],
+			[0, 0, c.MOVE, 0, 0]
+		]
+	},
+	{
+		school: 'crane',
+		color: 'dark-green',
+		player: c.BLUE,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, c.MOVE, 0],
+			[0, c.MOVE, c.START, 0, 0],
+			[0, 0, 0, c.MOVE, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'monkey',
+		color: 'dark-green',
+		player: c.BLUE,
+		card: [
+			[0, 0, 0, 0, 0],
 			[0, c.MOVE, 0, c.MOVE, 0],
 			[0, 0, c.START, 0, 0],
-			[0, 0, c.MOVE, 0, 0],
+			[0, c.MOVE, 0, c.MOVE, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'elephant',
+		color: 'dark-green',
+		player: c.RED,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, c.MOVE, c.MOVE, 0, 0],
+			[0, 0, c.START, 0, 0],
+			[0, c.MOVE, c.MOVE, 0, 0],
+			[0, 0, 0, 0, 0]
+		]
+	},
+	{
+		school: 'tiger',
+		color: 'dark-green',
+		player: c.RED,
+		card: [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0],
+			[c.MOVE, 0, c.START, c.MOVE, 0],
+			[0, 0, 0, 0, 0],
 			[0, 0, 0, 0, 0]
 		]
 	}
